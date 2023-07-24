05fba63176e2da0c8141d06257090bfec6010cce
LEESANGNAM <fgh0116@naver.com>
2023-07-24 11:11:01 +0900

    UserInteraction & TapGesture

--- Week2/ViewController.swift
+++ Week2/ViewController.swift
@@ -9,16 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("5678")
-        print("안녕안녕")
-        print("커밋과 동시에 푸쉬하기")
-        print("리드미 썼다 쀼듯")
         
+        print(firstImageView.isUserInteractionEnabled)
+        print(view.isUserInteractionEnabled)
     }
-
-
+    
+    
+    @IBAction func testFirstTapGesture(_ sender: UITapGestureRecognizer) {
+        print("first")
+    }
+    
+    @IBAction func testSecondTapGesture(_ sender: UITapGestureRecognizer) {
+        print("second")
+    }
+    
 }
 
