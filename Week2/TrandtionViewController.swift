5ba26518aa2b2e9246e0fd9bd16b33ab778fb0b3
LEESANGNAM <fgh0116@naver.com>
2023-07-25 12:32:06 +0900

    add Open Source packege

--- Week2/TrandtionViewController.swift
+++ Week2/TrandtionViewController.swift
@@ -6,19 +6,44 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TrandtionViewController: UIViewController {
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
+    @IBOutlet weak var actorImageView: UIImageView!
+    
+    
     @IBOutlet weak var userTextField: UITextField!
+    
+    let actor = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpImage(urlString:  "https://image.newsis.com/2023/05/04/NISI20230504_0001258444_web.jpg")
+       
     }
     
+    func setUpImage(urlString : String){
+        let url = URL(string: urlString)
+        actorImageView.kf.setImage(with: url)
+    }
+    @IBAction func segmentValueChanged(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == actor{
+            // http( apple block) - app Transport Secutity Setting
+            setUpImage(urlString:  "https://image.newsis.com/2023/05/04/NISI20230504_0001258444_web.jpg")
+           
+            
+        }else if sender.selectedSegmentIndex == 1{
+            setUpImage(urlString:  "https://i.namu.wiki/i/KHZxgx6dilwr4Z7uu6wSPoVlf5aIb6rq6qIOBV2LYBYdN9cWFaLlvkggojNNTD6mrwtGxS_lTPh4Woge2hzuZQ.webp")
+
+        }else{
+            print("오류")
+        }
+        
+    }
     //다른화면에서 transitionViewController로 돌아 올때 (unwind) 사용하는 기능
     // 액션연결 하지 않고, 직접 작성했습니다.
     @IBAction func unwindToHome(_ segue: UIStoryboardSegue){
